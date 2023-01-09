854f0aabadaf002f576204191e8486c3f197da02
Taerogrammer <hyung54321@naver.com>
2023-01-09 19:32:44 +0900

    feat(fe): fe-setting

---
+++ frontend/vite.config.ts
@@ -0,0 +1,25 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+import pages from "vite-plugin-pages";
+import icons from "unplugin-icons/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+
+    icons({ autoInstall: true }),
+
+    pages({
+      pagesDir: [],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
