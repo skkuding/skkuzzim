55632ea8176f3d289b9ce80f3070dec0c3d364dd
goo314 <rn0731@g.skku.edu>
2023-01-11 23:00:08 +0900

    chore: add margin to body in html

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,5 +3,16 @@
 </script>
 
 <template>
-  <router-view />
+  <!-- TODO: 헤더 컴포넌트 추가 -->
+  <main class="app-container">
+    <router-view />
+  </main>
 </template>
+
+<style scoped>
+.app-container {
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
