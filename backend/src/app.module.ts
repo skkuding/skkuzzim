28b48c232f569f0677893cdc6d6cd1a89cfc1ad3
SH9480P <realsehwan@gmail.com>
2023-01-12 23:07:33 +0900

    fix: add configmodule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ReservationModule } from './reservation/reservation.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ReservationModule, PrismaModule],
+  imports: [
+    ReservationModule,
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
