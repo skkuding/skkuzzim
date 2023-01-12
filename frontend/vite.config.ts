634493534b99cb42b694154f8a3a79f1895e3b67
goo314 <rn0731@g.skku.edu>
2023-01-12 19:39:31 +0900

    chore: delete page directory option

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,15 +8,7 @@ import icons from "unplugin-icons/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-
-    icons({ autoInstall: true }),
-
-    pages({
-      pagesDir: [{ dir: "src/pages", baseRoute: "" }],
-    }),
-  ],
+  plugins: [vue(), icons({ autoInstall: true }), pages()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
