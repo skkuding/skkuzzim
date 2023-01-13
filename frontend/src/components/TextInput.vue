a35a6d86c6cce105ca612d5e726d97ae6ad26f76
Eunbi <jenabill@naver.com>
2023-01-13 21:14:23 +0900

    refactor(fe): update text input

--- frontend/src/components/TextInput.vue
+++ frontend/src/components/TextInput.vue
@@ -1,75 +1,44 @@
 <script setup lang="ts">
 import { COLOR, FONT_SIZE } from "@/styles/theme";
-import IconCircleXmark from "~icons/fa6-solid/circle-xmark";
-import { computed } from "vue";
-
-const props = defineProps<{
+defineProps<{
   placeholder?: string;
-  message: string;
-  removeButton?: boolean;
+  message?: string;
   modelValue: string;
 }>();
-
-const emit = defineEmits<{
-  (e: "remove"): void;
+defineEmits<{
   (e: "update:modelValue", value: string): void;
 }>();
-
-const model = computed({
-  get() {
-    return props.modelValue;
-  },
-  set(value) {
-    emit("update:modelValue", value);
-  },
-});
 </script>
 
 <template>
-  <div class="wrapper">
-    <div class="input-box">
-      <input type="text" v-model="model" :placeholder="placeholder || ''" />
-      <button type="button" v-if="removeButton" @click="$emit('remove')">
-        <IconCircleXmark />
-      </button>
-    </div>
-    <p>{{ message ? `*${message}` : "" }}</p>
+  <div>
+    <input
+      type="text"
+      :placeholder="placeholder"
+      :value="modelValue"
+      @input="
+        $emit('update:modelValue', ($event.target as HTMLInputElement).value)
+      "
+    />
+    <p v-if="message">* {{ message }}</p>
   </div>
 </template>
 
 <style scoped>
-.wrapper {
+div {
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   gap: 3px;
 }
-.input-box {
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid #000;
-}
 input {
   border: none;
   outline: none;
   font-size: 16px;
-  flex-grow: 1;
+  border-bottom: 1px solid #000;
 }
 p {
   font-size: v-bind("FONT_SIZE.description");
   color: v-bind("COLOR.red");
-}
-button {
-  background-color: transparent;
-  border: none;
-  outline: none;
-  padding: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-}
-button svg {
-  color: v-bind("COLOR.gray");
 }
 </style>
