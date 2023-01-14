de11f76ddf94effdb01cbffecd553fe868eb10f9
Eunbi <jenabill@naver.com>
2023-01-14 23:16:27 +0900

    refactor(fe): update text input

--- frontend/src/components/TextInput.vue
+++ frontend/src/components/TextInput.vue
@@ -1,25 +1,27 @@
 <script setup lang="ts">
 import { COLOR, FONT_SIZE } from "@/styles/theme";
-defineProps<{
+import { computed } from "vue";
+const props = defineProps<{
   placeholder?: string;
   message?: string;
   modelValue: string;
 }>();
-defineEmits<{
+const emit = defineEmits<{
   (e: "update:modelValue", value: string): void;
 }>();
+const model = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    emit("update:modelValue", value);
+  },
+});
 </script>
 
 <template>
   <div>
-    <input
-      type="text"
-      :placeholder="placeholder"
-      :value="modelValue"
-      @input="
-        $emit('update:modelValue', ($event.target as HTMLInputElement).value)
-      "
-    />
+    <input type="text" :placeholder="placeholder" v-model="model" />
     <p v-if="message">* {{ message }}</p>
   </div>
 </template>
@@ -35,7 +37,7 @@ input {
   border: none;
   outline: none;
   font-size: 16px;
-  border-bottom: 1px solid #000;
+  border-bottom: 1px solid black;
 }
 p {
   font-size: v-bind("FONT_SIZE.description");
