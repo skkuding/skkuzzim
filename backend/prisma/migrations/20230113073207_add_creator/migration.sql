0eb445034cc1a5296ccad213fb33f5e86fd9f269
jspark2000 <idpjs2000@gmail.com>
2023-01-16 00:48:33 +0900

    delete prisma/migrations

--- backend/prisma/migrations/20230113073207_add_creator/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `creator` to the `reservation` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "reservation" ADD COLUMN     "creator" TEXT NOT NULL;
