4b7d92ba670869f55740949d1eaa300732797fcf
jspark2000 <idpjs2000@gmail.com>
2023-01-16 01:04:55 +0900

    전체 예약정보 조회 완성

--- backend/src/reservation/reservation.controller.ts
+++ backend/src/reservation/reservation.controller.ts
@@ -1,34 +1,15 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
-import { ReservationService } from './reservation.service';
+import { Controller, Get, Query } from '@nestjs/common'
+import { ReservationService } from './reservation.service'
 
 @Controller('reservation')
 export class ReservationController {
   constructor(private readonly reservationService: ReservationService) {}
 
   @Get()
-  async getReservation(
+  async getReservations(
     @Query('startTime') startTime: string,
-    @Query('endTime') endTime: string,
+    @Query('endTime') endTime: string
   ) {
-    return await this.reservationService.getAllReservation(startTime, endTime);
-  }
-
-  @Post()
-  async createReservation(
-    @Body('name') name: string,
-    @Body('club') club: string,
-    @Body('startTime') startTime: string,
-    @Body('endTime') endTime: string,
-    @Body('purpose') purpose: string,
-    @Body('member') member: string[],
-  ) {
-    await this.reservationService.createReservation(
-      name,
-      club,
-      startTime,
-      endTime,
-      purpose,
-      member,
-    );
+    return await this.reservationService.getAllReservations(startTime, endTime)
   }
 }
--- backend/src/reservation/reservation.service.ts
+++ backend/src/reservation/reservation.service.ts
@@ -1,27 +1,27 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
 
-const HALF_HOUR = 30 * 60 * 1000;
+const HALF_HOUR = 30 * 60 * 1000
 
 @Injectable()
 export class ReservationService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async getAllReservation(startTime: string, endTime: string) {
+  async getAllReservations(startTime: string, endTime: string) {
     const result = await this.prisma.reservation.findMany({
       where: {
         AND: [
           {
             startTime: {
-              gte: new Date(startTime),
-            },
+              gte: new Date(startTime)
+            }
           },
           {
             endTime: {
-              lte: new Date(endTime),
-            },
-          },
-        ],
+              lte: new Date(endTime)
+            }
+          }
+        ]
       },
       select: {
         startTime: true,
@@ -29,21 +29,21 @@ export class ReservationService {
         club: true,
         member: {
           select: {
-            username: true,
-          },
-        },
+            username: true
+          }
+        }
       },
       orderBy: {
-        startTime: 'asc',
-      },
-    });
+        startTime: 'asc'
+      }
+    })
 
     const splitedReservationList: {
-      startTime: Date;
-      endTime: Date;
-      club: string;
-      people: number;
-    }[] = [];
+      startTime: Date
+      endTime: Date
+      club: string
+      people: number
+    }[] = []
 
     result.forEach((item) => {
       for (
@@ -55,71 +55,42 @@ export class ReservationService {
           startTime: new Date(base),
           endTime: new Date(base + HALF_HOUR),
           club: item.club,
-          people: item.member.length,
-        });
+          people: item.member.length
+        })
       }
-    });
+    })
 
     const mergedReservationList = splitedReservationList.map(
       (item, index, array) => {
         const a = array.filter(
-          (value) => value.startTime.getTime() === item.startTime.getTime(),
-        );
+          (value) => value.startTime.getTime() === item.startTime.getTime()
+        )
         return {
           startTime: item.startTime,
           endTime: item.endTime,
           skkuding: a.reduce(
             (accum, object) =>
               object.club === 'skkuding' ? accum + object.people : accum + 0,
-            0,
+            0
           ),
           skku_d: a.reduce(
             (accum, object) =>
               object.club === 'skku_d' ? accum + object.people : accum + 0,
-            0,
+            0
           ),
           isFull:
             a.reduce((accum, object) => (accum += object.people), 0) < 8
               ? false
-              : true,
-        };
-      },
-    );
+              : true
+        }
+      }
+    )
 
-    const mergedReservationMap = new Map();
+    const mergedReservationMap = new Map()
     mergedReservationList.forEach((item) => {
-      mergedReservationMap.set(JSON.stringify(item), item);
-    });
+      mergedReservationMap.set(JSON.stringify(item), item)
+    })
 
-    return new Array(...mergedReservationMap.values());
-  }
-
-  async createReservation(
-    name: string,
-    club: string,
-    startTime: string,
-    endTime: string,
-    purpose: string,
-    member: string[],
-  ) {
-    const memberObj = member.map((item) => {
-      return { username: item };
-    });
-    return await this.prisma.reservation.create({
-      data: {
-        startTime: new Date(startTime),
-        endTime: new Date(endTime),
-        club,
-        purpose,
-        member: {
-          create: [
-            {
-              username: name,
-            },
-            ...memberObj,
-          ],
-        },
-      },
-    });
+    return new Array(...mergedReservationMap.values())
   }
 }
