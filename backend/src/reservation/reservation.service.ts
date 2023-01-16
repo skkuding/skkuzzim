23c4817ed811ce819e7f82f2dd84488d6cb730e5
jspark2000 <idpjs2000@gmail.com>
2023-01-16 17:30:24 +0900

    change variable names

--- backend/src/reservation/reservation.service.ts
+++ backend/src/reservation/reservation.service.ts
@@ -62,24 +62,27 @@ export class ReservationService {
 
     const mergedReservationList = splitedReservationList.map(
       (item, index, array) => {
-        const a = array.filter(
+        const overlabedReservations = array.filter(
           (value) => value.startTime.getTime() === item.startTime.getTime()
         )
         return {
           startTime: item.startTime,
           endTime: item.endTime,
-          skkuding: a.reduce(
+          skkuding: overlabedReservations.reduce(
             (accum, object) =>
               object.club === 'skkuding' ? accum + object.people : accum + 0,
             0
           ),
-          skkud: a.reduce(
+          skkud: overlabedReservations.reduce(
             (accum, object) =>
               object.club === 'skkud' ? accum + object.people : accum + 0,
             0
           ),
           isFull:
-            a.reduce((accum, object) => (accum += object.people), 0) < 8
+            overlabedReservations.reduce(
+              (accum, object) => (accum += object.people),
+              0
+            ) < 8
               ? false
               : true
         }
