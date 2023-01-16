d2d96252b125c69c47c641ab8dfc559385446ccc
Eunbi <jenabill@naver.com>
2023-01-16 15:05:14 +0900

    refactor(fe): remove v-model, add width style

--- frontend/src/components/TextInput.vue
+++ frontend/src/components/TextInput.vue
@@ -1,27 +1,25 @@
 <script setup lang="ts">
-import { COLOR, FONT_SIZE } from "@/styles/theme";
-import { computed } from "vue";
-const props = defineProps<{
-  placeholder?: string;
-  message?: string;
-  modelValue: string;
-}>();
-const emit = defineEmits<{
-  (e: "update:modelValue", value: string): void;
-}>();
-const model = computed({
-  get() {
-    return props.modelValue;
-  },
-  set(value) {
-    emit("update:modelValue", value);
-  },
-});
+import { COLOR, FONT_SIZE } from '@/styles/theme'
+defineProps<{
+  placeholder?: string
+  message?: string
+  modelValue: string
+}>()
+defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
 </script>
 
 <template>
   <div>
-    <input type="text" :placeholder="placeholder" v-model="model" />
+    <input
+      type="text"
+      :placeholder="placeholder"
+      :value="modelValue"
+      @input="
+        $emit('update:modelValue', ($event.target as HTMLInputElement).value)
+      "
+    />
     <p v-if="message">* {{ message }}</p>
   </div>
 </template>
@@ -32,15 +30,17 @@ div {
   flex-direction: column;
   align-items: flex-start;
   gap: 3px;
+  width: 100%;
 }
 input {
+  width: 100%;
   border: none;
   outline: none;
   font-size: 16px;
   border-bottom: 1px solid black;
 }
 p {
-  font-size: v-bind("FONT_SIZE.description");
-  color: v-bind("COLOR.red");
+  font-size: v-bind('FONT_SIZE.description');
+  color: v-bind('COLOR.red');
 }
 </style>
