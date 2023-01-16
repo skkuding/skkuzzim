0853d1dc0af17e80f9d09177ce19fdc0d9c1903c
Hyeongrok1 <loggy0323@g.skku.edu>
2023-01-17 00:43:23 +0900

    feat: made reservation update service

--- backend/src/reservation/reservation.service.ts
+++ backend/src/reservation/reservation.service.ts
@@ -1,7 +1,13 @@
-import { Injectable } from '@nestjs/common'
+import { Body, Injectable } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
 export class ReservationService {
   constructor(private readonly prismaService: PrismaService) {}
+
+  async updateReservation(@Body() body: any) {
+    await this.prismaService.update_reservation(body.data.id, body.data.creator, body.data.club, body.data.startTime, body.data.endTime, body.data.purpose);
+    await this.prismaService.existing_member_delete(body.data.id);
+    await this.prismaService.update_member(body.data.id, body.data.member);
+  }
 }
