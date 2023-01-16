936f837357197ae730aa19c9f838d4329dc16791
Hyeongrok1 <loggy0323@g.skku.edu>
2023-01-17 00:45:41 +0900

    feat: made prisma update service

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient();
+
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor(private config: ConfigService) {
@@ -13,4 +15,48 @@ export class PrismaService extends PrismaClient {
       }
     })
   }
+
+  async update_reservation(
+    request_id: number,
+    request_creator: string, 
+    request_club: string,
+    request_startTime: Date, 
+    request_endTime: Date,
+    request_purpose: string
+  ) {
+    return prisma.reservation.update({
+      where: {
+        id: request_id
+      },
+      data: {
+        id: request_id,
+        creator: request_creator,
+        club: request_club,
+        startTime: request_startTime,
+        endTime: request_endTime,
+        purpose: request_purpose,
+        updateTime: new Date(),
+      }
+    });
+  }
+
+  async existing_member_delete(request_id: number) {
+    return await prisma.member.delete({
+      where: {
+        reservationId: request_id,
+      }
+    });
+  }
+
+  async update_member(request_id: number, memberlist: string[]) {
+    let data_array: any;
+    for (let i = 0; i < memberlist.length; i++) {
+      data_array.push({reservationId: request_id, username: memberlist[i]});
+    }
+
+    return await prisma.member.createMany({
+      data: data_array,
+    });
+  }
+  
 }
