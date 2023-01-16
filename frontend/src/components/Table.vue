cfb977d0a5b28cb8046001e77d8223638020d1fe
goo314 <rn0731@g.skku.edu>
2023-01-17 02:51:16 +0900

    refactor: compute date with use-date-format in vueuse

--- frontend/src/components/Table.vue
+++ frontend/src/components/Table.vue
@@ -1,100 +1,81 @@
 <script setup lang="ts">
-import { useNow } from '@vueuse/core'
+import { useDateFormat, useNow } from '@vueuse/core'
 import { COLOR, FONT_SIZE } from '@/styles/theme'
-import { ref } from 'vue'
-
-interface TimeRange {
-  startTime: {
-    hour: number
-    minute: number
-  }
-  endTime: {
-    hour: number
-    minute: number
-  }
-}
+import { computed, ref } from 'vue'
 
 const props = defineProps<{
-  startTime: Date
+  monday: Date
 }>()
 
-// 현재 날짜 및 시각
+type Day = {
+  day: string
+  date: string // YYYY-MM-DD
+  style?: 'current' | 'sat' | 'sun'
+}
+
 const now = useNow()
-const isTodayIncluded = ref(false)
-const isSameDate = (date1: Date, date2: Date) => {
-  return (
-    date1.getFullYear() === date2.getFullYear() &&
-    date1.getMonth() === date2.getMonth() &&
-    date1.getDate() === date2.getDate()
-  )
-}
+const isThisWeek = ref(false)
 
-// row header
-const DAYS = ['월', '화', '수', '목', '금', '토', '일']
-const DATE_MS = 86400000
-const today = new Date()
-const dates: Date[] = []
-for (let i = 0; i < 7; i++) {
-  const date = new Date(props.startTime.getTime() + DATE_MS * i)
-  dates.push(date)
-  if (isSameDate(date, today)) {
-    isTodayIncluded.value = true
-  }
-}
+// 헤더: 일주일 동안의 요일과 날짜, css 스타일 정보
+const week = computed(() => {
+  const DAYS = ['월', '화', '수', '목', '금', '토', '일']
+  const format = 'YYYY-MM-DD'
+  return new Array<Day>(7)
+    .fill({
+      day: '월',
+      date: useDateFormat(props.monday, format).value
+    })
+    .map((x, i) => {
+      const temp = Object.assign({}, x)
+      temp.day = DAYS[i]
+      temp.date = useDateFormat(
+        new Date().setDate(props.monday.getDate() + i),
+        format
+      ).value
+      temp.style =
+        temp.date === useDateFormat(now, format).value
+          ? 'current'
+          : temp.day === '토'
+          ? 'sat'
+          : temp.day === '일'
+          ? 'sun'
+          : temp.style
+      if (temp.style === 'current') isThisWeek.value = true
+      return temp
+    })
+})
 
-// column header
-const HOURS = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
-const MINUTES = [0, 30]
-const TIMES: Array<TimeRange> = []
-for (let i = 1; i <= 30; i++) {
-  TIMES.push({
-    startTime: {
-      hour: HOURS[Math.floor((i - 1) / 2)],
-      minute: MINUTES[Math.floor((i - 1) % 2)]
-    },
-    endTime: {
-      hour: HOURS[Math.floor(i / 2)],
-      minute: MINUTES[Math.floor(i % 2)]
+// 행: 시작시간과 끝시간
+const HOURS = [
+  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24
+].map((x) => x.toString().padStart(2, '0'))
+const MINUTES = ['00', '30']
+const PERIODS = new Array(30)
+  .fill({
+    start: '00:00',
+    end: '24:00'
+  })
+  .map((x, i) => {
+    return {
+      start: `${HOURS[Math.floor(i / 2)]}:${MINUTES[Math.floor(i % 2)]}`,
+      end: `${HOURS[Math.floor((i + 1) / 2)]}:${
+        MINUTES[Math.floor((i + 1) % 2)]
+      }`
     }
   })
-}
-const getTimeRangeString = ({ startTime, endTime }: TimeRange) => {
-  return `${startTime.hour}:${startTime.minute.toString().padStart(2, '0')}~${
-    endTime.hour
-  }:${endTime.minute.toString().padStart(2, '0')}`
-}
 
-const isCurrentTime = ({ startTime, endTime }: TimeRange) => {
-  if (isTodayIncluded.value) {
-    if (
-      startTime.hour === now.value.getHours() &&
-      startTime.minute <= now.value.getMinutes()
-    ) {
-      if (endTime.minute === 0) return true
-      if (now.value.getMinutes() <= endTime.minute - 1) return true
-      return false
-    }
-    return false
-  }
-  return false
-}
-
-const getStartTimeString = (
-  date: Date,
-  time: { hour: number; minute: number }
-) => {
-  const datetime = new Date(
-    date.getFullYear(),
-    date.getMonth(),
-    date.getDate(),
-    time.hour,
-    time.minute,
-    0,
-    0
+const isNow = (start: string) => {
+  if (!isThisWeek.value) return false
+  const nowH = now.value.getHours()
+  const nowM = now.value.getMinutes()
+  const startH = parseInt(start.substring(0, 2))
+  const startM = parseInt(start.substring(2, 4))
+  if (
+    (startH === nowH && startM === 0 && nowM < 30) ||
+    (startH === nowH && startM === 30 && 30 < nowM && nowM < 60)
   )
-  return new Date(
-    datetime.getTime() - datetime.getTimezoneOffset() * 60000
-  ).toJSON()
+    return true
+  return false
 }
 </script>
 
@@ -102,34 +83,28 @@ const getStartTimeString = (
   <table>
     <thead>
       <tr>
-        <th></th>
-        <th v-for="(date, index) in dates" :key="index" class="row">
-          <span
-            :class="
-              isSameDate(now, date)
-                ? 'today'
-                : DAYS[index] === '토'
-                ? 'sat'
-                : DAYS[index] === '일'
-                ? 'sun'
-                : ''
-            "
-          >
-            <span class="day">{{ DAYS[index] }}</span>
-            <span class="date">{{ date.getDate() }}</span>
+        <th />
+        <th
+          v-for="({ day, date, style }, index) in week"
+          :key="index"
+          class="row"
+        >
+          <span :class="style">
+            <span class="day">{{ day }}</span>
+            <span class="date">{{ useDateFormat(date, 'DD').value }}</span>
           </span>
         </th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="(timeRange, index) in TIMES" :key="index">
+      <tr v-for="({ start, end }, index) in PERIODS" :key="index">
         <th class="column">
-          <span :class="isCurrentTime(timeRange) ? 'today' : ''">
-            {{ getTimeRangeString(timeRange) }}
+          <span :class="isNow(start) ? 'current' : ''">
+            {{ start }} ~ {{ end }}
           </span>
         </th>
-        <td v-for="(date, index) in dates" :key="index">
-          <slot :name="getStartTimeString(date, timeRange.startTime)"></slot>
+        <td v-for="({ date }, index) in week" :key="index">
+          <slot :name="`${date}T${start}:00.000Z`" />
         </td>
       </tr>
     </tbody>
@@ -142,17 +117,17 @@ table {
   font-size: v-bind('FONT_SIZE.content');
   border-spacing: 0;
 }
-.today {
+.current {
   padding: 9px 5px;
   color: white;
   border-radius: 12px;
   background-color: v-bind('COLOR.yellow');
 }
-th.row .today {
+th.row .current {
   max-width: 64px;
   margin: 0 auto;
 }
-th.column .today {
+th.column .current {
   padding: 4px 6px;
 }
 .sat {
