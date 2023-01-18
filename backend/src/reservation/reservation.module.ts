379c04b63b74ffcdb155f7d0821a06ebf07f2d91
k1g99 <idmatthew9546@gmail.com>
2023-01-18 22:04:46 +0900

    refactor(be): remove prismaservice provider from reservation module

--- backend/src/reservation/reservation.module.ts
+++ backend/src/reservation/reservation.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common'
-import { PrismaService } from 'src/prisma/prisma.service'
 import { ReservationController } from './reservation.controller'
 import { ReservationService } from './reservation.service'
 
 @Module({
   controllers: [ReservationController],
-  providers: [ReservationService, PrismaService]
+  providers: [ReservationService]
 })
 export class ReservationModule {}
