72c3c8d6d1fd037ac380a0a6651d746ccf45efa7
Hyeongrok1 <105476324+Hyeongrok1@users.noreply.github.com>
2023-01-19 01:28:33 +0900

    fix: fix update reservation error

--- backend/src/reservation/reservation.service.ts
+++ backend/src/reservation/reservation.service.ts
@@ -1,13 +1,49 @@
-import { Body, Injectable } from '@nestjs/common'
+import { updateReservationDto } from './reservation.dto'
+import { Injectable } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
 export class ReservationService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async updateReservation(@Body() body: any) {
-    await this.prismaService.update_reservation(body.data.id, body.data.creator, body.data.club, body.data.startTime, body.data.endTime, body.data.purpose);
-    await this.prismaService.existing_member_delete(body.data.id);
-    await this.prismaService.update_member(body.data.id, body.data.member);
+  async updateReservation(body: updateReservationDto) {
+    const { id, creator, club, startTime, endTime, purpose, member } = body
+
+    if (member.length > 8) {
+      throw new Error('member number is more than 8')
+    }
+
+    const data_array = member.map((element) => {
+      return { reservationId: id, username: element }
+    })
+
+    const deleteMany = await this.prismaService.member.deleteMany({
+      where: {
+        reservationId: {
+          equals: id
+        },
+      }
+    })
+
+    const member_add = await this.prismaService.member.createMany({
+      data: [...data_array]
+    })
+
+    const reservation_update = await this.prismaService.reservation.update({
+      where: {
+        id: id,
+      },
+      data: {
+        id: id,
+        creator: creator,
+        club: club,
+        startTime: startTime,
+        endTime: endTime,
+        purpose: purpose,
+        updateTime: new Date(),
+      }
+    })
+
+    return reservation_update
   }
 }
