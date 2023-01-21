5b4da3f9a652de076d2eb8a832c6e04010947f1b
EliseCho13 <dbwlspdlqj@naver.com>
2023-01-21 18:54:21 +0900

    feat: modal box done

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
 // write script code
+import Modal from './components/Modal.vue'
 </script>
 
 <template>
   <!-- TODO: 헤더 컴포넌트 추가 -->
+  <Modal title="예약자 정보 수정">
+    <div id="modalContentTest">
+      <!-- slot에 임시로 넣어둔 content -->
+      <div>
+        <label for="name">예약자 이름</label>
+        <input v-model="name" placeholder="name" />
+      </div>
+      <div>
+        <label for="people">인원</label>
+        <input v-model="people" placeholder="people" />
+      </div>
+    </div>
+  </Modal>
   <main class="wrapper">
     <router-view />
   </main>
@@ -15,5 +29,11 @@
   max-width: 80rem;
   padding-left: 5rem;
   padding-right: 5rem;
+}
+#modalContentTest {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-end;
 }
 </style>
---
+++ frontend/src/components/Modal.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { onClickOutside } from '@vueuse/core'
+import Button from './Button.vue';
+const modal = ref(false)
+const modalRef = ref(null)
+
+onClickOutside(
+  modalRef,
+  (event) => {
+    console.log(event)
+    modal.value = false
+  },
+)
+defineProps<{
+  title: string;
+  content?: string;
+}>()
+
+</script>
+
+<template>
+  <button @click="modal = true">
+    Open Modal
+  </button>
+  <div v-if="modal" ref="modalRef" class="modal">
+    <div class="inner">
+      <p class="heading">
+        {{ title }}
+      </p>
+      <div id="contentContainer">
+        <slot />
+      </div>
+      <div id="buttonContainer">
+        <Button color="red">취소</Button>
+        <Button color="green">생성</Button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.modal {
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 100%;
+  z-index: 10;
+}
+.inner {
+  background-color: white;
+  padding: 24px 48px;
+  border-radius: 5px;
+  border: 1px solid lightgrey;
+  box-shadow: 2px 2px 10px rgba(10, 10, 10, 0.1);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.heading {
+  font-weight: bold;
+  font-size: 1.4rem;
+}
+#buttonContainer {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+#contentContainer {
+  margin: 20px;
+}
+Button {
+  margin: 0 15px;
+}
+input {
+  border: none;
+  border-bottom: 1px solid black;
+}
+</style>
