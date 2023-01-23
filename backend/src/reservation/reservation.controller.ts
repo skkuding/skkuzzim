78c8fbcad35f08ffd22bd83d7c4fe91fbc932958
Jisoo Park <102132161+jspark2000@users.noreply.github.com>
2023-01-23 23:11:17 +0900

    feat(BE): add get all reservations service (#39)
    * Made DTO for Response and Request.
    
    * 전체 예약 정보 조회 기능 구현
    
    * delete package-lock.json
    
    * delete prisma/migrations
    
    * 전체 예약정보 조회 완성
    
    * change 'skku_d' to 'skkud'
    
    * 전체 파일 prettier 적용
    
    * change variable names
    
    * modify query
    
    * add class validation
    
    * Revert "delete prisma/migrations"
    
    This reverts commit 0eb445034cc1a5296ccad213fb33f5e86fd9f269.
    
    * fix(be): resolve corner case
    
    전체 예약 조회시 조회 범위보다 예약의 길이가 길 경우 불러오지 못하는 문제를 해결하였습니다.
    
    
    * fix: pnpm-lock.yaml
    
    * refactor(be): add type
    
    메소드 반환 타입 지정 및 타입 추론이 안되는 변수에 타입 지정
    
    
    * refactor(be): change name prisma to prismaService
    
    변수명 prisma를 prismaService로 변경합니다.
    
    
    * fix(be): add response dto
    
    전체 예약 정보 조회 요청에 대한 응답 DTO를 추가합니다.
    
    
    Co-authored-by: JungangHighschool <lhspanzer@중앙고컴퓨터실.localdomain>
    Co-authored-by: goo314 <rn0731@g.skku.edu>
    
    Close #3

--- backend/src/reservation/reservation.controller.ts
+++ backend/src/reservation/reservation.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Query } from '@nestjs/common'
+import { GetAllReservationDTO } from './dto/getAllReservation.dto'
+import { ReservationService } from './reservation.service'
 
 @Controller('reservation')
-export class ReservationController {}
+export class ReservationController {
+  constructor(private readonly reservationService: ReservationService) {}
+
+  @Get()
+  async getReservations(@Query() reservationDTO: GetAllReservationDTO) {
+    return await this.reservationService.getAllReservations(reservationDTO)
+  }
+}
--- backend/src/reservation/reservation.service.ts
+++ backend/src/reservation/reservation.service.ts
@@ -1,7 +1,119 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { GetAllReservationDTO } from './dto/getAllReservation.dto'
+import { RespondGetAllReservationRequestDTO } from './dto/respondGetAllReservationRequest.dto'
+
+const HALF_HOUR = 30 * 60 * 1000
 
 @Injectable()
 export class ReservationService {
   constructor(private readonly prismaService: PrismaService) {}
+
+  async getAllReservations(
+    reservationDTO: GetAllReservationDTO
+  ): Promise<RespondGetAllReservationRequestDTO[]> {
+    const { startTime, endTime } = reservationDTO
+    const result = await this.prismaService.reservation.findMany({
+      where: {
+        OR: [
+          {
+            AND: [
+              { startTime: { gte: new Date(startTime) } },
+              { startTime: { lt: new Date(endTime) } }
+            ]
+          },
+          {
+            AND: [
+              { endTime: { gt: new Date(startTime) } },
+              { endTime: { lte: new Date(endTime) } }
+            ]
+          },
+          {
+            AND: [
+              { startTime: { lte: new Date(startTime) } },
+              { endTime: { gte: new Date(endTime) } }
+            ]
+          }
+        ]
+      },
+      select: {
+        startTime: true,
+        endTime: true,
+        club: true,
+        member: {
+          select: {
+            username: true
+          }
+        }
+      },
+      orderBy: {
+        startTime: 'asc'
+      }
+    })
+
+    const splitedReservationList: {
+      startTime: Date
+      endTime: Date
+      club: string
+      people: number
+    }[] = []
+
+    result.forEach((item) => {
+      for (
+        let base = item.startTime.getTime();
+        base < item.endTime.getTime();
+        base += HALF_HOUR
+      ) {
+        if (
+          base >= new Date(startTime).getTime() &&
+          base < new Date(endTime).getTime()
+        ) {
+          splitedReservationList.push({
+            startTime: new Date(base),
+            endTime: new Date(base + HALF_HOUR),
+            club: item.club,
+            people: item.member.length
+          })
+        }
+      }
+    })
+
+    const mergedReservationList: RespondGetAllReservationRequestDTO[] =
+      splitedReservationList.map((item, index, array) => {
+        const overlabedReservations = array.filter(
+          (value) => value.startTime.getTime() === item.startTime.getTime()
+        )
+        return {
+          startTime: item.startTime,
+          endTime: item.endTime,
+          skkuding: overlabedReservations.reduce(
+            (accum, object) =>
+              object.club === 'skkuding' ? accum + object.people : accum + 0,
+            0
+          ),
+          skkud: overlabedReservations.reduce(
+            (accum, object) =>
+              object.club === 'skkud' ? accum + object.people : accum + 0,
+            0
+          ),
+          isFull:
+            overlabedReservations.reduce(
+              (accum, object) => (accum += object.people),
+              0
+            ) < 8
+              ? false
+              : true
+        }
+      })
+
+    const mergedReservationMap: Map<
+      string,
+      RespondGetAllReservationRequestDTO
+    > = new Map()
+    mergedReservationList.forEach((item) => {
+      mergedReservationMap.set(JSON.stringify(item), item)
+    })
+
+    return new Array(...mergedReservationMap.values())
+  }
 }
