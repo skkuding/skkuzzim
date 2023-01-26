ad155f73e4bf43d8bcce116d518b5e25f4247098
EliseCho13 <dbwlspdlqj@naver.com>
2023-01-26 20:37:51 +0900

    fix: made header props simpler

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,47 +1,50 @@
 <script setup lang="ts">
 import IconBrandsGithub from "~icons/fa6-brands/github";
-const props = defineProps<{
-  year?: number;
-  month: number;
-  day?: number;
-  startTime?: number;
-  endTime?: number;
+defineProps<{
+  dayTime: string;
 }>();
-// console.log("time is", props.month);
 </script>
 
 <template>
-  <header class="headerWrapper">
+  <header class="header-wrapper">
     <!-- <div
       class="letters"
       v-if="startTime && endTime ? `${{ year }} year` : `${{ month }} month`"
     ></div> -->
-    <div class="letters" v-if="startTime && endTime && !year && day">
-      {{ month }}월 {{ day }}일 {{ startTime }} ~ {{ endTime }}
-    </div>
-    <div class="letters" v-if="year && !day && !startTime && !endTime">
-      {{ year }}년 {{ month }}월
-    </div>
-    <div>
-      <a href="https://github.com/skkuding/skkuzzim" target="_blank">
-        <IconBrandsGithub class="letters" />
-      </a>
+    <div class="header-content-wrapper">
+      <div class="letters">
+        {{ dayTime }}
+      </div>
+      <div>
+        <a href="https://github.com/skkuding/skkuzzim" target="_blank">
+          <IconBrandsGithub class="letters" />
+        </a>
+      </div>
     </div>
   </header>
 </template>
 
 <style scoped>
-.headerWrapper {
+.header-wrapper {
   width: 100%;
   background: linear-gradient(90deg, #8dc63f, #3f9dc6);
   color: white;
   display: flex;
-  justify-content: space-around;
   align-items: center;
   height: 80px;
   position: sticky;
   top: 0;
   margin: 0;
+  padding: 0 5rem;
+}
+.header-content-wrapper {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 80rem;
+  margin: 0 auto;
 }
 .letters {
   color: white;
