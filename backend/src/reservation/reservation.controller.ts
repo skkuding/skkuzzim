cbd3fb6a0ae0bbcb59cc2ef0bb5fd17381cfeef3
Minhyuk Kim <rhrlwld@gmail.com>
2023-01-26 16:28:28 +0900

    refactor: add type conversion pipe

--- backend/src/reservation/reservation.controller.ts
+++ backend/src/reservation/reservation.controller.ts
@@ -1,12 +1,12 @@
 import { ReservationService } from './reservation.service'
-import { Controller, Delete, Param } from '@nestjs/common'
+import { Controller, Delete, Param, ParseIntPipe } from '@nestjs/common'
 
 @Controller('reservation')
 export class ReservationController {
   constructor(private readonly reservationService: ReservationService) {}
 
   @Delete('/:id')
-  async deleteReservation(@Param('id') id: number) {
-    return this.reservationService.deleteReservation(id)
+  async deleteReservation(@Param('id', ParseIntPipe) id: number) {
+    return await this.reservationService.deleteReservation(id)
   }
 }
