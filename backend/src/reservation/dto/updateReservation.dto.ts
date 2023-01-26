bdff670ca4988cbb1414a0ec69d659756e3fa57e
Se Hwan <realsehwan@gmail.com>
2023-01-27 01:13:16 +0900

    refactor: make UpdateReservationDto more simple

--- backend/src/reservation/dto/updateReservation.dto.ts
+++ backend/src/reservation/dto/updateReservation.dto.ts
@@ -1,22 +1,3 @@
-import { IsString, IsDateString, IsOptional } from 'class-validator'
+import { CreateReservationRequestDto } from './createReservation.dto'
 
-export class UpdateReservationDto {
-  @IsString()
-  creator: string
-
-  @IsString()
-  club: string
-
-  @IsDateString()
-  startTime: Date
-
-  @IsDateString()
-  endTime: Date
-
-  @IsString()
-  @IsOptional()
-  purpose: string
-  
-  @IsString({ each: true })
-  members: string[]
-}
+export class UpdateReservationDto extends CreateReservationRequestDto {}
